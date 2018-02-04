27af11f7533858efa762d0d41248a783d08edd81
julien <julien.bernard.iphone@gmail.com>
2018-02-04 18:56:22 +0100

    return None if no results

--- application/services/datareader.py
+++ application/services/datareader.py
@@ -13,25 +13,30 @@ class DatareaderService(object):
     def select(self, query, parameters=None, fetchone=False, limit=50):
         cursor = self.connection.cursor()
 
-        performed_query = '{} LIMIT {}'.format(query, limit)
+        if limit > 0:
+            performed_query = '{} LIMIT {}'.format(query, limit)
+        else:
+            performed_query = query
 
         if parameters is None:
             cursor.execute(performed_query)
         else:
             cursor.execute(performed_query, parameters)
 
-        meta = [r[0] for r in cursor.description]
+        result = None
+        if cursor.description:
+            meta = [r[0] for r in cursor.description]
 
-        if fetchone:
-            raw_data = cursor.fetchone()
-            results = dict(zip(meta, raw_data))
-        else:
-            raw_data = cursor.fetchall()
+            if fetchone:
+                raw_data = cursor.fetchone()
+                results = dict(zip(meta, raw_data))
+            else:
+                raw_data = cursor.fetchall()
 
-            results = list()
+                results = list()
 
-            for r in raw_data:
-                results.append(dict(zip(meta, r)))
+                for r in raw_data:
+                    results.append(dict(zip(meta, r)))
 
         cursor.close()
 
